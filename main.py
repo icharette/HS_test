2930ba13882f8692dfb56bdb2fed5045e3da3644
icharette <35855975+icharette@users.noreply.github.com>
2024-05-04 13:01:21 -0400

    found location of tables for Closing Information, Transaction Informationm Loan Information

--- main.py
+++ main.py
@@ -8,16 +8,51 @@ def get_data(file_name):
     table_names = {"Closing Information","Transaction Information","Loan Information"}
 
     #get list of tables returned from camelot read pdf method, for all pages
-    tables = camelot.read_pdf(pdf_path, flavor='stream', pages="1-5")
+    tables = camelot.read_pdf(pdf_path, flavor='stream', pages="1")
 
     #output tables
-    for table in tables:
-        print("---------------------------------------------------")
+    #for table in tables:
+        #print("---------------------------------------------------")
         #the table obj needs to be converted to a data frame for wanted output
-        print(table.df)
+        #print(table.df)
     return tables
+
+def get_coordinates(table_name, tables):
+    if tables:
+        #the table name we are looking for is in the first table
+        table = tables [0]
+        df = table.df
+        #flag to keep track if we found the location or not
+        value_found = False
+
+        # Iterate over the DataFrame to find the table name and its position
+        for i, row in enumerate(df.values):
+            for j, cell in enumerate(row):
+                if cell == table_name:
+                    print(f"Value '{table_name}' found at row {i} and column {j}.")
+                    value_found = True
+                    return i,j
+                    break
+            if value_found:
+                break
+        
+        #IF we haven't found the table name
+        if not value_found:
+            print(f"Value '{table_name}' not found in the table.")
+            return None, None
 
 
 if __name__ == "__main__":
    pdf_path = "Closing_Disclosure.pdf"
-   data = get_data(pdf_path)
+   tables = get_data(pdf_path)
+   table_names = {"Closing Information","Transaction Information","Loan Information"}
+
+   #find location of tables: "Closing Information","Transaction Information","Loan Information"
+   if tables:
+       for name in table_names:
+           row, column = get_coordinates(name, tables)
+           print("row: ", row)
+           print("column: ", column)
+   else:
+       print("no tables found")
+       #need to add unit test for this
