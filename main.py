b6dbbdfc0bd6b3d626d87ee88a26ce3da51f654f
icharette <35855975+icharette@users.noreply.github.com>
2024-05-04 13:40:14 -0400

    Need to fix formatting for some rows that the values span over several lines

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_coordinates(table_name, tables):
         for i, row in enumerate(df.values):
             for j, cell in enumerate(row):
                 if cell == table_name:
-                    print(f"Value '{table_name}' found at row {i} and column {j}.")
+                    #print(f"Value '{table_name}' found at row {i} and column {j}.")
                     value_found = True
                     return i,j
                     break
@@ -41,41 +41,45 @@ def get_coordinates(table_name, tables):
             print(f"Value '{table_name}' not found in the table.")
             return None, None
 
-def parse_table_date(table_names, row_start, df):
-    
-    i = 0
-    #iterate through the dictionary of {"Closing Information":{}, "Transaction Information": {}, "Loan Information":{}}
-    for tableKey, tableValue in table_names.items():
-        #iterate through the table data of the rows starting beneath the table titles, in each column (i) accordingly (which corresponds to each table of: {"Closing Information":{}, "Transaction Information": {}, "Loan Information":{}})
-        for index, value in df.iloc[row_start:, i].items():
-           # print("index: ", index)
-            #print("value: ", value)
+def parse_table_data(tableKey, tableValue, table_names, row_start, df,i):
+   #iterate through the table data of the rows starting beneath the table titles, in each column (i) accordingly (which corresponds to each table of: {"Closing Information":{}, "Transaction Information": {}, "Loan Information":{}})
+    for index, value in df.iloc[row_start:, i].items():
+        # print("index: ", index)
+        #print("value: ", value)
 
-            #checking if value to avoid keeping track of empty spaces
-            if value:
-                #add to the table_names dictionnary with the table values assigned to each table element in table_names
-                #check if line return in value and if so, split the strings to seperate key(column) and value in each table
-                if '\n' in value:
-                    key, value = value.split('\n')
-                    table_names[tableKey][key] = value
-        i = i + 1
-    print(table_names)
+        #checking if value to avoid keeping track of empty spaces
+        if value:
+            #add to the table_names dictionnary with the table values assigned to each table element in table_names
+            #check if line return in value and if so, split the strings to seperate key(column) and value in each table
+            if '\n' in value:
+                key, value = value.split('\n')
+                table_names[tableKey][key] = value
+    
+    
+    #print(table_names)
 
 if __name__ == "__main__":
    pdf_path = "Closing_Disclosure.pdf"
    tables = get_data(pdf_path)
-   table_names = {"Closing Information","Transaction Information","Loan Information"}
+   #table_names = {"Closing Information","Transaction Information","Loan Information"}
    table_dict  = {"Closing Information":{}, "Transaction Information": {}, "Loan Information":{}}
 
    #find location of tables: "Closing Information","Transaction Information","Loan Information"
    if tables:
-       for name in table_names:
-           row, column = get_coordinates(name, tables)
-           print("row: ", row)
-           print("column: ", column)
+        #using the value of i to index through the table title index (seen as a column in the data frame output from the camelot table extraction)
+        i = 0
+        for tableKey, tableValue in table_dict.items():
+           row, column = get_coordinates(tableKey, tables)
+           #print("row: ", row)
+           #print("column: ", column)
 
            #get data for each table:
-           parse_table_date(table_dict, row + 1, tables[0].df)
+           #iterate through the dictionary of {"Closing Information":{}, "Transaction Information": {}, "Loan Information":{}}
+           parse_table_data(tableKey, tableValue, table_dict, row + 1, tables[0].df,i)
+           i = i + 1
+        print(table_dict)
    else:
        print("no tables found")
        #need to add unit test for this
+
+    
