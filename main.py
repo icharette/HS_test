e4f8dee4aaa91d8c530e7d8e10f91ffb402c16de
icharette <35855975+icharette@users.noreply.github.com>
2024-05-04 12:46:46 -0400

    initial commit. added a git ignore to not add the pdf to the remote repository

---
+++ main.py
@@ -0,0 +1,4 @@
+#Context: Technical test backend python: extract specific information from a PDF file and present the data in a structured format (JSON, SQLITE3)
+#Methods: main, get_data, parse_data, write_json, write_db, unit_tests
+
+
